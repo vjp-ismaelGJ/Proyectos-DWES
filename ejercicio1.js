09d7a121d82de92951220902e7cf6617845e2fd8
IsmaelGJ <igilj04@informatica.iesvalledeljerteplasencia.es>
2025-09-16 17:30:52 +0200

    borracion

--- ejercicio1.js
+++
@@ -1,4 +0,0 @@
-alert("Bienvenido");
-var nombre = prompt("Introduce un nombre de usuario: ");
-console.log("Ha entrado en el sistema " + nombre);
-document.writeln('<h1>Bienvenido, ' + nombre + '</h1>');
