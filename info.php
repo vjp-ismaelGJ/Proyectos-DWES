76782daf05e8d671b855bf17927f50a2caacec3d
IsmaelGJ <igilj04@informatica.iesvalledeljerteplasencia.es>
2025-09-16 18:50:23 +0200

    comentarios

--- info.php
+++ info.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
+    phpinfo();
+    // Apache Version	Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.2.12
+
+    /* Loaded Modules	core mod_win32 mpm_winnt http_core mod_so mod_access_compat mod_actions mod_alias mod_allowmethods mod_asis mod_auth_basic mod_authn_core mod_authn_file mod_authz_core mod_authz_groupfile mod_authz_host mod_authz_user mod_autoindex mod_cgi mod_dav_lock mod_dir mod_env mod_headers mod_include mod_info mod_isapi mod_log_config mod_cache_disk mod_mime mod_negotiation mod_proxy mod_proxy_ajp mod_rewrite mod_setenvif mod_socache_shmcb mod_ssl mod_status mod_version mod_php */
+
+    // Memory limit     512M
+
+    // $_SERVER['DOCUMENT_ROOT']	C:/xampp/htdocs
 ?>
+
